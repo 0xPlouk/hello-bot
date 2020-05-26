a81ee9823ea98210f87adb0971d20b3523bc3fad
Subnetix <57098204+Subnetix@users.noreply.github.com>
2020-05-26 12:13:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,8 +4,8 @@ const bot = new Discord.Client()
 bot.on('ready', function () {
     console.log("Connected !")
 })
-bot.login('NzE0NDE2MzY0NDExOTQ0OTk4.Xszpmg.31iypl_sktAqSnLOTD5P-BkU5nM')
+bot.login('toekn')
 
 bot.on("guildMemberAdd", member => {
-    member.guild.channels.cache.get('613123692813352970').send(`Bienvenue ${member.user} tu peux te présenter ici :slight_smile:`);
+    member.guild.channels.cache.get('613123692813352970').send(`Welcom ${member.user}`);
 });
